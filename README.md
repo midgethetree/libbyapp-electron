09a314b2583f7439b93820449b718163ba7da164
midgethetree <76988393+midgethetree@users.noreply.github.com>
2024-03-09 15:34:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Libby
+
+An Electron-based desktop application for [Libby](https://libbyapp.com/), the free library reading app that lets you access thousands of digital books and audiobooks from your local library.
---
+++ main.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron')
+const path = require('path')
+
+let mainWindow
+
+const iconPath = path.join(__dirname, 'libby.png')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 800,
+    icon: nativeImage.createFromPath(iconPath),
+    autoHideMenuBar: true,
+    backgroundColor: '#000000'
+  })
+
+  win.on('minimize',function(event){
+    event.preventDefault();
+    win.hide();
+  });
+
+  win.on('close', (event) => {
+    event.preventDefault()
+    win.hide()
+  })
+
+  win.loadURL('https://libbyapp.com')
+
+  return win
+}
+
+app.whenReady().then(() => {
+
+  mainWindow = createWindow()
+
+  const icon = nativeImage.createFromPath(iconPath)
+  tray = new Tray(icon)
+
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Quit', click: () => app.exit(0)}
+  ])
+
+  tray.setContextMenu(contextMenu)
+  tray.setToolTip('Libby')
+  tray.setTitle('Libby')
+  tray.on('click', () => {
+    if (mainWindow) {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide()
+      }
+      else {
+        mainWindow.show()
+      }
+    }
+    else {
+      mainWindow = createWindow()
+    }
+  })
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      mainWindow = createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
